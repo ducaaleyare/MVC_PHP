89ec262bc1c5f8224fa9c75761510f93805a7c41
ducaaleyare <ducaaleyare400@gmail.com>
2024-12-10 02:35:45 +0300

    Removig comments from the ControllerTest.php

--- tests/Unit/ControllerTest.php
+++ tests/Unit/ControllerTest.php
@@ -4,13 +4,13 @@ use App\Controllers\HomeController;
 use App\View;
 
 test('HomeController', function () {
-    // Create an instance of the HomeController
+
     $controller = new HomeController;
 
-    // Call the index method
     $response = $controller->index();
+
     $expect = View::make('index');
 
-    // Check if the response is the expected view
+
     $this->assertEquals($response, $expect);
 });
